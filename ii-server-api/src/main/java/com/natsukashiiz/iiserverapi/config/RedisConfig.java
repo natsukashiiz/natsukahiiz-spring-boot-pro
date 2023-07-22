da02f09fecf6e27c0f970974a93a0fa728ee0e62
vv2dev <74323401+natsukashiiz@users.noreply.github.com>
2023-07-22 16:38:38 +0700

    added(redis):config

---
+++ ii-server-api/src/main/java/com/natsukashiiz/iiserverapi/config/RedisConfig.java
@@ -0,0 +1,29 @@
+package com.natsukashiiz.iiserverapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+
+@Configuration
+@ComponentScan("com.natsukashiiz.iiserverapi")
+@EnableRedisRepositories(basePackages = "com.natsukashiiz.iiserverapi.repository")
+@PropertySource("classpath:application.yml")
+public class RedisConfig {
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        final RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setValueSerializer(new GenericToStringSerializer<>(Object.class));
+        return template;
+    }
+}
