c4fb05bf0c59a36aaaab74d2960ea5614cf0e0ce
vv2dev <74323401+natsukashiiz@users.noreply.github.com>
2023-07-24 13:42:16 +0700

    updated-(Pagination ):docs

--- ii-common/src/main/java/com/natsukashiiz/iicommon/model/Pagination.java
+++ ii-common/src/main/java/com/natsukashiiz/iicommon/model/Pagination.java
@@ -2,6 +2,12 @@ package com.natsukashiiz.iicommon.model;
 
 import lombok.Data;
 
+/**
+ * {
+ *    "page": 1,
+ *    "limit": 10
+ * }
+ */
 @Data
 public class Pagination {
     private Integer page = 0;
