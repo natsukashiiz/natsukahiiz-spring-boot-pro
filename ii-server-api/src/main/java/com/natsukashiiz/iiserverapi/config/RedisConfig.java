3365a158ae68fe4e9c009007839aa8aecb03917d
vv2dev <74323401+natsukashiiz@users.noreply.github.com>
2023-07-24 10:03:21 +0700

    updated(redis):import

--- ii-server-api/src/main/java/com/natsukashiiz/iiserverapi/config/RedisConfig.java
+++ ii-server-api/src/main/java/com/natsukashiiz/iiserverapi/config/RedisConfig.java
@@ -1,13 +1,9 @@
 package com.natsukashiiz.iiserverapi.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
